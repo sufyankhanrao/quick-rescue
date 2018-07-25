90d2586f3187a6920c283a1c6c8eb9f24c7fa4c3
Muhammad Sufyan <msufyan@GlobalRescue.local>
2018-07-26 02:28:20 +0500

    Comments updated

--- QuickRescue/src/main/java/com/gr/assignments/DAOLayer/IManager.java
+++ QuickRescue/src/main/java/com/gr/assignments/DAOLayer/IManager.java
@@ -3,15 +3,15 @@ package com.gr.assignments.DAOLayer;
 import java.util.List;
 
 public interface IManager {
-	// Add new Account in the database
+	// Add Details in the database
 	public int addDetails(Object object);
 
-	// Update an existing Account in the database with new Account
+	// Update Details in the database
 	public Object updateDetails(int ID, Object object);
 
-	// Delete an existing Account from the database
+	// Delete Details from the database
 	public Object deleteDetails(int ID);
 
-	// View all existing Accounts in the database
+	// View all existing Details in the database
 	public List<Object> viewAllDetails();
 }
