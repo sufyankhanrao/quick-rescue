1c321cc9c31831cfcef143dee6467625105b8421
Muhammad Sufyan <msufyan@GlobalRescue.local>
2018-08-09 14:05:47 +0500

    modified

--- Task 2 and 3/qrapi/src/main/webapp/assets/js/br.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-	
-	$.each(businessRules, function(i, item) {
-		$('#businessRuleTable tbody').append('<tr><td>' + (i + 1) + '</td><td>' + item.code + '</td><td>' + item.message + '</td><td>' + item.br + '</td></tr>');
-	});
-	
-})
